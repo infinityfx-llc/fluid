616367a5861c62a9fab7265ca88598877aed5331
Florian Lopez Plaza <contact@infinity-fx.com>
2022-11-03 12:18:26 +0100

    Implemented Timeline attributes: uniform, horizontal and progress.

--- packages/fluid/src/components/feedback/static/timeline/index.js
+++ packages/fluid/src/components/feedback/static/timeline/index.js
@@ -1,18 +1,18 @@
 import React, { Children } from 'react';
-import { combine, mergeFallback } from '@core/utils';
+import { mergeFallback } from '@core/utils';
 import useStyles from '@hooks/styles';
 import defaultStyles from './style';
 
-export default function Timeline({ children, styles, uniform }) {
+export default function Timeline({ children, styles, uniform, horizontal, progress }) {
     const style = useStyles(mergeFallback(styles, defaultStyles));
     children = Children.toArray(children);
 
-    return <div className={style.timeline} data-uniform={uniform}>
+    return <div className={style.timeline} data-uniform={uniform} data-horizontal={horizontal}>
         {children.map((child, i) => {
             return <div key={i} className={style.event}>
                 <div className={style.line}>
-                    <div className={style.bullet} />
-                    {i < children.length - 1 && <div className={style.segment} />}
+                    <div className={style.bullet} data-active={i < progress} />
+                    {i < children.length - 1 && <div className={style.segment} data-active={i < progress - 1} />}
                 </div>
                 {child}
             </div>;
@@ -22,5 +22,9 @@ export default function Timeline({ children, styles, uniform }) {
 
 Timeline.defaultProps = {
     styles: {},
-    uniform: true
+    uniform: false,
+    horizontal: false,
+    progress: 0
 };
+
+// maybe make animated
--- packages/fluid/src/components/feedback/static/timeline/style.js
+++ packages/fluid/src/components/feedback/static/timeline/style.js
@@ -3,8 +3,16 @@ export default {
         display: 'grid'
     },
 
-    '.timeline[data-uniform="true"]': {
+    '.timeline[data-uniform="true"][data-horizontal="false"]': {
         gridAutoRows: '1fr'
+    },
+
+    '.timeline[data-uniform="true"][data-horizontal="true"]': {
+        gridAutoColumns: '1fr'
+    },
+
+    '.timeline[data-horizontal="true"]': {
+        gridAutoFlow: 'column'
     },
     
     '.event': {
@@ -14,28 +22,62 @@ export default {
         alignItems: 'start'
     },
 
-    '.event:not(:last-child)': {
+    '.timeline[data-horizontal="true"] .event': {
+        flexDirection: 'column'
+    },
+
+    '.timeline[data-horizontal="false"] .event:not(:last-child)': {
         paddingBottom: 'var(--fluid-gap-med)'
+    },
+
+    '.timeline[data-horizontal="true"] .event:not(:last-child)': {
+        paddingRight: 'var(--fluid-gap-med)'
     },
 
     '.line': {
         display: 'flex',
         justifyContent: 'center',
+        alignItems: 'center'
     },
 
     '.segment': {
         position: 'absolute',
         width: '4px',
-        height: 'calc(100% + 1rem)',
+        height: '100%',
+        top: '1rem',
         backgroundColor: 'var(--fluid-clr-grey-300)'
     },
 
+    '.timeline[data-horizontal="true"] .segment': {
+        width: '100%',
+        height: '4px',
+        top: 'auto',
+        left: '1rem'
+    },
+
     '.bullet': {
+        position: 'relative',
         width: 'calc(2rem - 4px)',
         height: 'calc(2rem - 4px)',
         backgroundColor: 'var(--fluid-clr-background-100)',
         border: 'solid 4px var(--fluid-clr-grey-300)',
         borderRadius: '999px',
         zIndex: 1
+    },
+
+    '.bullet[data-active="true"]': {
+        borderColor: 'var(--fluid-clr-primary-100)'
+    },
+
+    '.bullet[data-active="true"]::after': {
+        content: '""',
+        position: 'absolute',
+        inset: '3px',
+        backgroundColor: 'var(--fluid-clr-primary-100)',
+        borderRadius: '999px'
+    },
+
+    '.segment[data-active="true"]': {
+        backgroundColor: 'var(--fluid-clr-primary-100)'
     }
 }
