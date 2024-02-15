18f17e2cd6d8de0be6d4a83a0bf58fe023fcff41
Florian Lopez Plaza <contact@infinity-fx.com>
2024-02-15 23:06:41 +0100

    Updated dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,7 @@ const plugins = [
     preserveDirectives()
 ];
 
-const external = ['react', /react-dom/, 'react/jsx-runtime', /@infinityfx\/lively/];
+const external = ['react', 'react-dom', 'react/jsx-runtime', /@infinityfx\/lively/, 'tslib'];
 const onwarn = (msg, handler) => {
     if (msg.code === 'THIS_IS_UNDEFINED') return;
     if (msg.code === 'MODULE_LEVEL_DIRECTIVE') return;
