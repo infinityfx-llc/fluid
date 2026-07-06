2b0a3027d96db29c3968830c5e701ac71ca11145
Florian Lopez Plaza <contact@infinity-fx.com>
2026-07-06 16:50:47 +0200

    Changed default value for ActionMenu autofocus.

--- src/components/display/action-menu/root.tsx
+++ src/components/display/action-menu/root.tsx
@@ -4,13 +4,13 @@ import MenuManager from "../../../context/menu-manager";
 import Popover from "../../layout/popover";
 import type { PopoverRoot } from "../../layout/popover/root";
 
-export default function Root({ round, variant, autoFocus = true, ...props }: Omit<PopoverRoot, 'mobileContainer'> & {
+export default function Root({ round, variant, autoFocus = false, ...props }: Omit<PopoverRoot, 'mobileContainer'> & {
     round?: boolean;
     variant?: 'default' | 'inverted';
     /**
      * Focus the first item when opening the menu.
      * 
-     * @default true
+     * @default false
      */
     autoFocus?: boolean;
 }) {
